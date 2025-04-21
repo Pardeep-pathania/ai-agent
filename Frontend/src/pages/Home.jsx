371963be923560627811c9621c58a3c19dff59c3
pardeep-pathania <pathanianikhil145@gmail.com>
2025-04-21 23:23:07 +0530

    logic to showing projects on the backend

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,47 +1,81 @@
-import React, { useState } from 'react'
-import { UserContext } from '../context/user.context'
-import { useContext } from 'react';
-import axios from '../config/axios';
-
-
+import React, { useState } from "react";
+import { UserContext } from "../context/user.context";
+import { useContext, useEffect } from "react";
+import axios from "../config/axios";
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
-
   const { user } = useContext(UserContext); // Assuming you have a UserContext to manage user state
 
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [projectName, setProjectName] = useState(null)
+  const [projectName, setProjectName] = useState(null);
+  const [project, setProject] = useState([]);
 
-  function createProject(e){
+  const navigate = useNavigate()
+
+  function createProject(e) {
     e.preventDefault();
 
-    console.log({projectName})
+    console.log({ projectName });
 
-    axios.post('/projects/create',{
-      name: projectName,
-     
-    })
-    .then((res) => {
-      console.log(res)
-      setIsModalOpen(false)
-      setProjectName('')
-     
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+    axios
+      .post("/projects/create", {
+        name: projectName,
+      })
+      .then((res) => {
+        console.log(res);
+        setIsModalOpen(false);
+        setProjectName("");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
-  return (
-    <main className='p-4'>
+  useEffect(() => {
+    axios
+      .get("/projects/all")
+      .then((res) => {
+        console.log(res.data);
 
-      <div className="projects">
-        <button 
-        onClick={() => setIsModalOpen(true)}
-        className="project p-3 border border-slate-400 bg-gray-200 rounded-md shadow-xl">
+        setProject(res.data.projects);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <main className="p-4">
+      <div className="projects flex flex-wrap gap-3">
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="project p-3 border border-slate-400 bg-gray-200 rounded-md shadow-xl mb-2"
+        >
           New Project
-        <i className="ri-link ml-1"></i>
+          <i className="ri-link ml-1"></i>
         </button>
+
+        {project.map((project) => (
+          <div
+            key={project._id}
+            onClick={() => {
+              navigate(`/project`, {
+                state: { project },
+              });
+            }}
+            className="project flex flex-col gap-2 cursor-pointer p-4 border border-slate-300 rounded-md min-w-52 hover:bg-slate-200"
+          >
+            <h2 className="font-semibold">{project.name}</h2>
+
+            <div className="flex gap-2">
+              <p> <small>
+                <i className="ri-user-line"></i> Collaborators
+                </small>:</p>
+              {project.users.length}
+            </div>
+          </div>
+        ))}
       </div>
 
       {isModalOpen && (
@@ -54,7 +88,7 @@ const Home = () => {
                   Project Name
                 </label>
                 <input
-                onChange={(e) => setProjectName(e.target.value)}
+                  onChange={(e) => setProjectName(e.target.value)}
                   type="text"
                   className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
                   placeholder="Enter project name"
@@ -78,9 +112,8 @@ const Home = () => {
           </div>
         </div>
       )}
-
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
